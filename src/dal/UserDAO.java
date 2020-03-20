b4ed9f27fea9ba2bd9c0620cb52e063b6fda300c
Teridris <clementdupas@hotmail.fr>
2020-03-20 11:53:53 +0100

    clean DAO

--- src/dal/UserDAO.java
+++ src/dal/UserDAO.java
@@ -13,39 +13,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class UserDAO {
-    private static final String CREATE_QUERY = "INSERT INTO user (name, login, password) VALUES (?,?,?)";
-    private static final String FIND_ALL_QUERY = "SELECT * FROM user";
-    private static final String FIND_BY_ID = "SELECT * FROM user WHERE id = ?";
-    private static final String DELETE_QUERY = "DELETE FROM user WHERE id = ?";
     private static final String FIND_BY_LOG_AND_PWD = "SELECT * FROM user WHERE login = ? AND password = ?";
-    private static final String UPDATE_QUERY = "UPDATE user SET name = ?, login = ?, password = ? WHERE id = ?";
 
-
-    public User findById(int id) throws SQLException, ClassNotFoundException {
-        Connection connection = PersistenceManager.getConnection();
-        User user = new User();
-
-
-        if ( null != connection ) {
-            try ( PreparedStatement pst = connection.prepareStatement(FIND_BY_ID)) {
-                pst.setInt(1, id);
-                ResultSet rs = pst.executeQuery();
-                //int id = rs.getInt("id");
-                if (rs.next()){
-                    String name = rs.getString("name");
-                    String login = rs.getString("login");
-                    String password = rs.getString("password");
-                    user = new User(id, name, login, password);
-                    return user;
-                }
-
-
-            }
-
-        }
-        return user;
-
-    }
 
     public User login(String login, String password) throws SQLException, ClassNotFoundException, NoSuchAlgorithmException {
         Connection connection = PersistenceManager.getConnection();
@@ -70,43 +39,6 @@ public class UserDAO {
                 }
 
             }
-        return user;
-    }
-
-    public static Set<User> findAll() throws SQLException, ClassNotFoundException {
-        Connection connection = PersistenceManager.getConnection();
-        Set<User> usersList = new HashSet<>();
-        if ( null != connection ) {
-            try ( Statement st = connection.createStatement();
-                  ResultSet rs = st.executeQuery( FIND_ALL_QUERY )){
-                while ( rs.next() ) {
-                    int id = rs.getInt( "id" );
-                    String name = rs.getString( "name" );
-                    String login = rs.getString( "login" );
-                    String password = rs.getString( "password" );
-                    User user = new User(id, name, login, password);
-                    usersList.add( user );
-                }
-            }
-        }
-        return usersList;
-    }
-
-    public User update(int id, User user) throws SQLException, ClassNotFoundException {
-        Connection connection = PersistenceManager.getConnection();
-
-        if(null != connection) {
-            try (PreparedStatement pst = connection.prepareStatement(UPDATE_QUERY, Statement.RETURN_GENERATED_KEYS)){
-
-                pst.setString(1, user.getName());
-                pst.setString(2, user.getLogin());
-                pst.setString(3, user.getPassword());
-                pst.setInt(4, id);
-                pst.executeUpdate();
-
-
-            }
-        }
         return user;
     }
 
